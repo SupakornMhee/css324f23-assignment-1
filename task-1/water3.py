d2ed345eb99e04e086e9d81e9c16e7e68848448d
SupakornMhee <89035163+SupakornMhee@users.noreply.github.com>
2023-09-08 14:03:09 +0700

    Update water3.py
    update only initial state and goal check

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,10 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    a,b,c = s
+    return a==4 or b==4
 
 def successors(s):
-    x, y, z = s
+    a,b,c = s
     return []
